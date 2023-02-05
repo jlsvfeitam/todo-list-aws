359e228a23e744466b73fdd3d9b18b7081e6cf48
Ubuntu <ubuntu@ip-172-31-8-234.ec2.internal>
2023-02-05 16:53:27 +0000

    build.sh: add create file .aws/credentials with secrets

--- pipelines/common-steps/build.sh
+++ pipelines/common-steps/build.sh
@@ -2,5 +2,12 @@
 
 source todo-list-aws/bin/activate
 set -x
+mkdir .aws
+touch .aws/credentials
+echo "[default]" > .aws/credentials
+echo "aws_access_key_id = AKIAUNBGN4AV3CGB6WGZ" >> .aws/credentials
+echo "aws_secret_access_key = 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" >> .aws/credentials
+echo "aws_session_token =" >> .aws/credentials
+chmod 600 .aws/credentials
 sam validate --region us-east-1
 sam build
