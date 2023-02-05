644c2a56b4ad32d793972e6e8310133375efdf2b
Ubuntu <ubuntu@ip-172-31-8-234.ec2.internal>
2023-02-05 16:59:09 +0000

    build.sh: add create file .aws/credentials with secrets in HOME

--- pipelines/common-steps/build.sh
+++ pipelines/common-steps/build.sh
@@ -2,12 +2,13 @@
 
 source todo-list-aws/bin/activate
 set -x
-mkdir .aws
-touch .aws/credentials
-echo "[default]" > .aws/credentials
-echo "aws_access_key_id = AKIAUNBGN4AV3CGB6WGZ" >> .aws/credentials
-echo "aws_secret_access_key = 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" >> .aws/credentials
-echo "aws_session_token =" >> .aws/credentials
-chmod 600 .aws/credentials
+mkdir ${HOME}/.aws
+chmod 775 ${HOME}/.aws
+touch ${HOME}/.aws/credentials
+echo "[default]" > ${HOME}/.aws/credentials
+echo "aws_access_key_id = AKIAUNBGN4AV3CGB6WGZ" >> ${HOME}/.aws/credentials
+echo "aws_secret_access_key = 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" >> ${HOME}/.aws/credentials
+echo "aws_session_token =" >> ${HOME}/.aws/credentials
+chmod 600 ${HOME}/.aws/credentials
 sam validate --region us-east-1
 sam build
