8539e64e6805455ffb21766a475667e2f0295032
Ubuntu <ubuntu@ip-172-31-8-234.ec2.internal>
2023-02-06 07:05:33 +0000

    Remove secrets en build.sh and set as environment parameters

--- pipelines/common-steps/build.sh
+++ pipelines/common-steps/build.sh
@@ -6,8 +6,8 @@ mkdir ${HOME}/.aws
 chmod 775 ${HOME}/.aws
 touch ${HOME}/.aws/credentials
 echo "[default]" > ${HOME}/.aws/credentials
-echo "aws_access_key_id = AKIAUNBGN4AV3CGB6WGZ" >> ${HOME}/.aws/credentials
-echo "aws_secret_access_key = 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" >> ${HOME}/.aws/credentials
+echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" >> ${HOME}/.aws/credentials
+echo "aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" >> ${HOME}/.aws/credentials
 echo "aws_session_token =" >> ${HOME}/.aws/credentials
 chmod 600 ${HOME}/.aws/credentials
 sam validate --region us-east-1
