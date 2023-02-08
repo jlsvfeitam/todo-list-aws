3fcbab1f229ec757b67b264409ec652595e3adaa
Ubuntu <ubuntu@ip-172-31-8-234.ec2.internal>
2023-02-08 20:54:40 +0000

    translate: test unit develop

--- src/translatetext.py
+++ src/translatetext.py
@@ -2,12 +2,12 @@ import boto3
 
 
 def translatetext(text, sourcelanguage, targetlanguage):
-    translate = boto3.client(service_name='translate', 
+    translate = boto3.client(service_name='translate',
                              region_name='us-east-1',
                              use_ssl=True)
-    resultTranslate = translate.translate_text( \
-        Text=text, 
-        SourceLanguageCode=sourcelanguage, 
-        TargetLanguageCode=targetlanguage)
+    resultTranslate = \
+        translate.translate_text(Text=text,
+                                 SourceLanguageCode=sourcelanguage,
+                                 TargetLanguageCode=targetlanguage)
     print('text in translatetext:', resultTranslate.get('TranslatedText'))
     return resultTranslate.get('TranslatedText')
