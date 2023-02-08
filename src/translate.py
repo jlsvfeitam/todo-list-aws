b18726468dda3b688cc15019318bc656402c2b45
Ubuntu <ubuntu@ip-172-31-8-234.ec2.internal>
2023-02-08 07:35:50 +0000

    Code for translate develop

---
+++ src/translate.py
@@ -0,0 +1,20 @@
+import json
+import decimalencoder
+import todoList
+
+# https://programmingpercy.tech/blog/develop-and-debug-sam/
+def translate(event, context):
+    # create a response
+    item = todoList.get_translate_item(event['pathParameters']['id'], event['pathParameters']['language'])
+    if item:
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(item,
+                               cls=decimalencoder.DecimalEncoder)
+        }
+    else:
+        response = {
+            "statusCode": 404,
+            "body": ""
+        }
+    return response
